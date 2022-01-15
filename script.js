c80e47656a7d5091ac62ea2a1f370a24551a36a4
Fabi0o <marfab32@gmail.com>
2022-01-15 13:13:11 +0100

    add fucntion that will be wired to html calculator and add comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//basic mathematic operations for calculator to work
 function add(a, b) {
   let sum = a + b;
   return sum;
@@ -13,4 +14,22 @@ function multiply(a, b) {
 function division(a, b) {
   let fraction = a / b;
   return fraction;
+}
+//function that will be wired to html, takes operator and 2 numbers
+//and then calls one of the basic functions
+function operate(operator, a, b) {
+  let result;
+  if (operator == "+") {
+    result = add(a, b);
+    return result;
+  } else if (operator == "-") {
+    result = substract(a, b);
+    return result;
+  } else if (operator == "*") {
+    result = multiply(a, b);
+    return result;
+  } else if (operator == "/") {
+    result = division(a, b);
+    return result;
+  }
 }
