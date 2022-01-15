fb62f6cb1da190626c308ff64ea21f9fb5485d84
Fabi0o <marfab32@gmail.com>
2022-01-15 13:02:50 +0100

    creat script.js file and creat basic functions for all mathematic operation needed in the projet

---
+++ script.js
@@ -0,0 +1,16 @@
+function add(a, b) {
+  let sum = a + b;
+  return sum;
+}
+function substract(a, b) {
+  let difference = a - b;
+  return difference;
+}
+function multiply(a, b) {
+  let product = a * b;
+  return product;
+}
+function division(a, b) {
+  let fraction = a / b;
+  return fraction;
+}
